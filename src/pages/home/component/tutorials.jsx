99071a024514c742203abbe365d0f7acd0b0a12d
onkar <onkar.haldar@ens.enterprises>
2022-01-11 00:42:27 +0530

    err removed

--- src/pages/home/component/tutorials.jsx
+++ src/pages/home/component/tutorials.jsx
@@ -13,7 +13,7 @@ const Tutorial = ({ sectionname }) => {
                         return (
                             <div className="col-md-3" key={i} >
                                 <div className="card">
-                                    <img className="card-img-top" src={item?.url} alt="Card image cap" />
+                                    <img className="card-img-top" src={item?.url} alt="" />
                                     <div className="card-body">
                                         <h5 className="card-title">{item?.heading}</h5>
                                         <p className="card-text">S{item?.text}</p>
